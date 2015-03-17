27d8424745e7fa3b698b5e74771f266464cdeb4b
pdallmeyer <patricia.dallmeyer@gmail.com>
2015-03-17 15:53:28 -0400

    javascript working
    card selection works
    amex crave not working

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,1 +1,41 @@
-/* Your JavaScript goes here */
+
+var $amex = $('#amex'),
+	$discover = $('#discover'),
+	$visa = $('#visa'),
+	$mastercard = $('#mastercard');
+
+
+$(function() {
+    $('#card-number').validateCreditCard(function(result) {
+        console.log(result.card_type.name);
+
+   		if (result.card_type.name === "amex") {
+   			 $amex.attr('checked', true);
+   		}
+
+    	
+    	else if (result.card_type.name === "visa"){
+    		$visa.attr('checked', true);
+    	}
+
+    	else if (result.card_type.name === "discover") {
+    		$discover.attr('checked', true);
+    	}
+
+    	else if (result.card_type.name === "mastercard") {
+    		$mastercard.attr('checked', true);
+    	}
+	
+
+		else {
+			alert("Nope!");
+		}
+    });
+
+
+//function(change-crv){
+//	if $amex.attr('checked', true);
+//		crv.addClass('amex');}  
+
+
+});
